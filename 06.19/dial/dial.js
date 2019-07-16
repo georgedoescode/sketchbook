827305cdbe1eba5fca99a5d25ad6494a56971656
George Francis <georgedoescode@gmail.com>
2019-07-16 14:31:35 +0100

    add dial stuff responsive tc

--- 06.19/dial/dial.js
+++ 06.19/dial/dial.js
@@ -14,34 +14,63 @@ function drawDial(args) {
     },
     triangleSimple: (x, y, w, h) => {
       triangle(x, y, x + w / 2, y + h / 2, x + w, y);
+    },
+    randomChars: length => {
+      let result = "";
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
     }
   };
 
   // vars
   const { x: originX, y: originY, radius } = args;
-  const minPoints = 4;
-  const maxPoints = 16;
+
+  const minPoints = 2;
+  const maxPoints = 10;
   const PIOffset = PI / random(4, 5);
   const numPoints = helpers.randomEvenNumber(minPoints, maxPoints);
+
   // whole half or quater
   const pointIncrement = helpers.choice([1, 0.5, 0.25]);
   // choose what shape to draw for the main and sub dial points
   const primaryChoice = floor(random(0, 4));
   const secondaryChoice = floor(random(0, 4));
 
-  const ellipseSizePrimary = random(4, 8);
+  // secondary shapes are always smaller than primary
+
+  const ellipseSizePrimary = random(radius / 14, radius / 10);
   const ellipseSizeSecondary = ellipseSizePrimary * random(0.5, 1);
 
-  const markerDepthPrimray = random(24, 48);
-  const markerDepthSecondary = markerDepthPrimray * random(0.5, 1);
+  const markerDepthPrimary = random(radius / 4, radius / 2);
+  const markerDepthSecondary = markerDepthPrimary * random(0.5, 1);
 
-  const markerWidthPrimary = random(4, 6);
+  const markerWidthPrimary = random(radius / 14, radius / 10);
   const markerWidthSecondary = markerWidthPrimary * random(0.5, 1);
 
+  // which has a fill color?
   const fillPrimary = random() > 0.5;
   const fillSecondary = !fillPrimary;
 
-  const lineDivider = 1.125;
+  // the size of lines
+  const lineDivider = random(1.075, 1.125);
+
+  const pointMarker = floor(random(0, numPoints + 1));
+  const markerType = floor(random(0, 3));
+
+  const drawNums = random() > 0.5;
+  const isOdd = random() > 0.5;
+
+  const textDist = 1.25;
+  const fontSize = radius / 7;
+
+  const dialAcronymLength = floor(random(2, 4));
 
   const drawDot = (x, y, radius, shouldFill) => {
     push();
@@ -58,7 +87,7 @@ function drawDial(args) {
     rotate(a);
     noFill();
     if (shouldFill) fill(0);
-    rect(-width / 2, width / 2, width, height);
+    rect(-width / 2, 0, width, height);
     pop();
   };
 
@@ -69,12 +98,49 @@ function drawDial(args) {
     rotate(a);
     noFill();
     if (shouldFill) fill(0);
-    helpers.triangleSimple(-width / 2, width / 2, width, height);
+    helpers.triangleSimple(-width / 2, 0, width, height);
     pop();
   };
 
   const drawLine = (x, y) => {
     line(x, y, x / lineDivider, y / lineDivider);
+  };
+
+  const drawMarker = (x, y) => {
+    push();
+    // draw the inner circle
+    fill(0);
+    ellipse(0, 0, radius, radius);
+    let a = atan2(y, x) - HALF_PI;
+    if (random() > 0.5) {
+      fill(255);
+    } else {
+      noFill();
+      strokeWeight(2);
+      stroke(255);
+    }
+    switch (markerType) {
+      case 0:
+        let circRadius = random(radius / 12, radius / 8);
+        ellipse(x / 3, y / 3, circRadius, circRadius);
+        break;
+      case 1:
+        let triangleSize = random(radius / 10, radius / 6);
+        rotate(a);
+        helpers.triangleSimple(
+          -triangleSize / 2,
+          triangleSize / 2,
+          triangleSize,
+          radius / 2
+        );
+        break;
+      default:
+        let markerSize = random(radius / 14, radius / 10);
+        rotate(a);
+        rect(-markerSize / 2, markerSize / 2, markerSize, radius / 3);
+        break;
+    }
+    pop();
   };
 
   // return evenly spaced points around a circle, accounting of our 'offset' at the bottom, starting from 6 oClock (+ HALF_PI)
@@ -95,17 +161,31 @@ function drawDial(args) {
       let { x, y } = calcMappedCirclePos(i);
       x *= radius;
       y *= radius;
+      textAlign(CENTER);
+      textSize(fontSize);
       // draw 'main' markers
       if (i % 1 === 0) {
-        textAlign(CENTER);
-        text(i, x * 1.2, y * 1.2);
+        if (drawNums) {
+          if (isOdd) {
+            text(i + 1, x * textDist, y * textDist);
+          } else {
+            text(i, x * textDist, y * textDist);
+          }
+        } else {
+          text(
+            helpers.randomChars(dialAcronymLength),
+            x * textDist,
+            y * textDist
+          );
+        }
+
         switch (primaryChoice) {
           case 1:
             drawTriangle(
               x,
               y,
               markerWidthPrimary,
-              -markerDepthPrimray,
+              -markerDepthPrimary,
               fillPrimary
             );
             break;
@@ -114,12 +194,12 @@ function drawDial(args) {
               x,
               y,
               markerWidthPrimary,
-              -markerDepthPrimray,
+              -markerDepthPrimary,
               fillPrimary
             );
             break;
           case 3:
-            drawLine(x, y, markerDepthPrimray);
+            drawLine(x, y, markerDepthPrimary);
             break;
           default:
             drawDot(x, y, ellipseSizePrimary, fillPrimary);
@@ -153,6 +233,10 @@ function drawDial(args) {
             drawDot(x, y, ellipseSizeSecondary, fillSecondary);
             break;
         }
+      }
+
+      if (i === pointMarker) {
+        drawMarker(x, y);
       }
     }
   };
--- 06.19/dial/sketch.js
+++ 06.19/dial/sketch.js
@@ -1,15 +1,40 @@
+let cols;
+let rows;
+
+let colSize;
+let rowSize;
+
+let font;
+
+function preload() {
+  font = loadFont("fonts/B612Mono-Regular.ttf");
+}
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
   background(252);
   noLoop();
+
+  cols = 5;
+  rows = 5;
+
+  colSize = width / cols;
+  rowSize = height / rows;
 }
 
 function draw() {
   background(252);
-  // draw the dial!
-  drawDial({
-    x: width / 2,
-    y: height / 2,
-    radius: 100
-  });
+  textFont(font);
+
+  for (let x = 0; x < width; x += colSize) {
+    for (let y = 0; y < height; y += rowSize) {
+      // draw the dial!
+      rect(x, y, colSize, rowSize);
+      drawDial({
+        x: x + colSize / 2,
+        y: y + rowSize / 2,
+        radius: min(colSize, rowSize) / 3
+      });
+    }
+  }
 }
