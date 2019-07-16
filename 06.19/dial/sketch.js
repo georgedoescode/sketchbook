4460bb50fd4e75c4f5c5603785c5ab5db3f59a1a
George Francis <georgedoescode@gmail.com>
2019-07-16 16:28:08 +0100

    simplify grid, start thing about special characters

--- 06.19/dial/sketch.js
+++ 06.19/dial/sketch.js
@@ -1,9 +1,3 @@
-let cols;
-let rows;
-
-let colSize;
-let rowSize;
-
 let font;
 
 function preload() {
@@ -14,27 +8,16 @@ function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
   background(252);
   noLoop();
-
-  cols = 5;
-  rows = 5;
-
-  colSize = width / cols;
-  rowSize = height / rows;
 }
 
 function draw() {
   background(252);
   textFont(font);
 
-  for (let x = 0; x < width; x += colSize) {
-    for (let y = 0; y < height; y += rowSize) {
-      // draw the dial!
-      rect(x, y, colSize, rowSize);
-      drawDial({
-        x: x + colSize / 2,
-        y: y + rowSize / 2,
-        radius: min(colSize, rowSize) / 3
-      });
-    }
-  }
+  // draw the dial!
+  drawDial({
+    x: width / 2,
+    y: height / 2,
+    radius: 200
+  });
 }
