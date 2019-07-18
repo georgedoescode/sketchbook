b4955e65a7f2411380a505b2c3f4ea0431b7225f
George Francis <georgedoescode@gmail.com>
2019-07-18 16:39:24 +0100

    clean up grid code

---
+++ 06.19/subdividinggrid/grid.js
@@ -0,0 +1,89 @@
+class Grid {
+  constructor(x, y, w, h, limit) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.limit = limit;
+    this.subrect;
+    this.areas = [];
+  }
+
+  generate() {
+    this.subrect = new SubRect(0, 0, width, height, 0, this.limit, this.areas);
+    return this.areas;
+  }
+}
+
+class SubRect {
+  constructor(x, y, w, h, depth, limit, storageArray) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.areas = [];
+    this.depth = depth;
+    this.limit = limit;
+    this.storageArray = storageArray;
+
+    if (this.depth < this.limit) {
+      this.depth++;
+      this.subDivide();
+    } else {
+      const { x, y, w, h } = this;
+      this.storageArray.push({ x, y, w, h });
+    }
+  }
+
+  subDivide() {
+    const { x, y, w, h } = this;
+
+    if (random() > 0.5) {
+      this.splitVert(x, y, w, h);
+    } else {
+      this.splitHorizontal(x, y, w, h);
+    }
+  }
+
+  splitVert(x, y, w, h) {
+    this.rectLeft = new SubRect(
+      x,
+      y,
+      w / 2,
+      h,
+      this.depth,
+      this.limit,
+      this.storageArray
+    );
+    this.rectRight = new SubRect(
+      x + w / 2,
+      y,
+      w / 2,
+      h,
+      this.depth,
+      this.limit,
+      this.storageArray
+    );
+  }
+
+  splitHorizontal(x, y, w, h) {
+    this.rectTop = new SubRect(
+      x,
+      y,
+      w,
+      h / 2,
+      this.depth,
+      this.limit,
+      this.storageArray
+    );
+    this.rectBottom = new SubRect(
+      x,
+      y + h / 2,
+      w,
+      h / 2,
+      this.depth,
+      this.limit,
+      this.storageArray
+    );
+  }
+}
--- 06.19/subdividinggrid/sketch.js
+++ 06.19/subdividinggrid/sketch.js
@@ -1,69 +1,25 @@
-const areas = [];
+let grid;
+let gridAreas;
 
 function setup() {
-  createCanvas(500, 500);
-  background(250);
+  createCanvas(windowWidth, windowHeight);
   noLoop();
+
+  textAlign(CENTER);
+
+  // x, y, w, h, limit
+  grid = new Grid(0, 0, width, height, 4);
+  gridAreas = grid.generate();
 }
 
 function draw() {
   background(250);
-  let r = new SubRect(0, 0, width, height, 0);
 
-  areas.forEach((area, index) => {
+  gridAreas.map(area => {
     const { x, y, w, h } = area;
+    noFill();
     rect(x, y, w, h);
-    textAlign(CENTER);
-    text(index, x + w / 2, y + h / 2);
+    fill(0);
+    ellipse(x + w / 2, y + h / 2, 5, 5);
   });
-
-  console.log(areas);
-}
-
-// take a rect
-// split horizontally or vertically
-// store the resulting two rects
-// remove the parent rect
-// repeat
-
-class SubRect {
-  constructor(x, y, w, h, depth) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.areas = [];
-    this.depth = depth;
-    this.limit = 4;
-    this.minRectWidth = 0;
-    this.minRectHeight = 0;
-
-    if (this.depth < this.limit) {
-      this.depth++;
-      this.subDivide();
-    } else {
-      const { x, y, w, h } = this;
-      areas.push({ x, y, w, h });
-    }
-  }
-
-  subDivide() {
-    const { x, y, w, h } = this;
-
-    if (random() > 0.5) {
-      this.splitVert(x, y, w, h);
-    } else {
-      this.splitHorizontal(x, y, w, h);
-    }
-  }
-
-  splitVert(x, y, w, h) {
-    this.rectLeft = new SubRect(x, y, w / 2, h, this.depth);
-    this.rectRight = new SubRect(x + w / 2, y, w / 2, h, this.depth);
-  }
-
-  splitHorizontal(x, y, w, h) {
-    this.rectTop = new SubRect(x, y, w, h / 2, this.depth);
-    this.rectBottom = new SubRect(x, y + h / 2, w, h / 2, this.depth);
-  }
 }
